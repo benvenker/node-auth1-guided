dff89bb8695241a5d1e43bd6ceba8da92c670527
Ben Venker <bvemails@gmail.com>
2020-04-29 22:13:13 -0500

    check for session in users route

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -24,6 +24,7 @@ router.post("/login", (req, res) => {
   Users.findBy({ username })
     .then(([user]) => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = username;
         res.status(200).json({ message: "Welcome!" });
       } else {
         res.status(401).json({ message: "invalid creds" });
--- users/users-router.js
+++ users/users-router.js
@@ -3,11 +3,15 @@ const router = require("express").Router();
 const Users = require("./users-model.js");
 
 router.get("/", (req, res) => {
-  Users.find()
-    .then((users) => {
-      res.json(users);
-    })
-    .catch((err) => res.send(err));
+  if (req.session && req.session.user) {
+    Users.find()
+      .then((users) => {
+        res.json(users);
+      })
+      .catch((err) => res.send(err));
+  } else {
+    res.status(401).json({ message: "not logged in" });
+  }
 });
 
 module.exports = router;

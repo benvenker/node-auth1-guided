c360e7d8ad008ed36a05e1a2b8f0fa316f806bdf
Ben Venker <bvemails@gmail.com>
2020-04-29 22:01:26 -0500

    configure session object

--- api/server.js
+++ api/server.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require("express-session");
 
 const usersRouter = require("../users/users-router.js");
 const authRouter = require("../auth/auth-router.js");
+
 const server = express();
 
+const sessionConfig = {
+  name: "chocolate-chip",
+  secret: "mysepshulsecret",
+  cookie: {
+    maxAge: 3600 * 1000,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false, // should be true in prod
+  saveUninitialized: false,
+};
+
+// Global Middleware
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.use("/api/users", usersRouter);
 server.use("/api/auth", authRouter);

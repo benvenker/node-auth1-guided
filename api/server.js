fbcf1b90e69572a4f6abdd283cb11fb9a06037d7
Ben Venker <bvemails@gmail.com>
2020-04-29 22:33:32 -0500

    restrict routes

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const session = require("express-session");
+const restricted = require("../auth/auth-router");
 
 const usersRouter = require("../users/users-router.js");
 const authRouter = require("../auth/auth-router.js");
@@ -26,7 +27,7 @@ server.use(express.json());
 server.use(cors());
 server.use(session(sessionConfig));
 
-server.use("/api/users", usersRouter);
+server.use("/api/users", restricted, usersRouter);
 server.use("/api/auth", authRouter);
 server.get("/", (req, res) => {
   res.json({ api: "up" });
--- users/users-router.js
+++ users/users-router.js
@@ -1,17 +1,14 @@
 const router = require("express").Router();
+const restricted = require("../auth/restricted-middleware");
 
 const Users = require("./users-model.js");
 
-router.get("/", (req, res) => {
-  if (req.session && req.session.user) {
-    Users.find()
-      .then((users) => {
-        res.json(users);
-      })
-      .catch((err) => res.send(err));
-  } else {
-    res.status(401).json({ message: "not logged in" });
-  }
+router.get("/", restricted, (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => res.send(err));
 });
 
 module.exports = router;
